4f24dd863aa9c7270c95b4542429d13534a69db6
Yobel Tayson <rotkyobel@gmail.com>
2020-10-11 19:19:57 -0500

    modal platillo

--- src/Components/Dishes/UI/ModalCreate/index.js
+++ src/Components/Dishes/UI/ModalCreate/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import Modal from '../../../Utilities/Modal'
+import Input from '../../../Utilities/Input'
+import { Title, Form, AddMore, Actions } from "./styles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
 const ModalCreate = () => {
   return (
     <Modal title='Crear Platillo' width = '653px' textButton='Crear' active>
       <div>
-        CREAR PLATO
+        <Form>
+          <Input placeholder="Tipo de Comida"></Input>
+          <Input placeholder="Nombre de Comida"></Input>
+          <Input placeholder="Precio"></Input>
+          <Input placeholder="Descripción"></Input>
+        </Form>
+        <Actions>
+          <Title>Añadir Porción</Title>
+          <AddMore>
+            <FontAwesomeIcon icon={faPlus} />
+          </AddMore>
+        </Actions>
+        <Form style={{display:"flex"}}>
+          <Input type="text" placeholder="Nombre" />
+          <Input type="text" placeholder="Precio" />
+        </Form>
       </div>
     </Modal>
   );
---
+++ src/Components/Dishes/UI/ModalCreate/styles.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Title = styled.p`
+  margin-bottom: 20px;
+  color: #606060;
+  font-size:20px;
+  font-weight:bold;
+`;
+export const Form = styled.div`
+  margin-bottom: 40px;
+  margin: 20px;
+  & input {
+      margin: 5px;
+  }
+`;
+export const Actions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 20px;
+`;
+export const AddMore = styled.div`
+  height: 34px;
+  width: 34px;
+  background: #08c497;
+  border-radius:40px;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  color: white;
+  font-size:16px;
+`;
